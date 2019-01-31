ad0e8865c1e77fc14468389bf838179f60dd4f38
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-01-31 23:25:15 +0900

    There was an error in the code of the message, so we redoed it

--- app/models/massage.rb
+++ app/models/massage.rb
@@ -1,7 +1,7 @@
 class Massage < ApplicationRecord
   belongs_to :group
   belongs_to :user
-  mount_uploader :image, ImageUploader
 
   validates :content, presence: true, unless: :image?
+  mount_uploader :image, ImageUploader
 end
