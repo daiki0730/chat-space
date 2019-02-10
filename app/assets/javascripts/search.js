09866da5f2fe9b9370c451ef0f4ef44f4309f784
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-02-10 21:07:22 +0900

    Add processing
    Process added to chat members when pushing add button

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -7,22 +7,23 @@ $(function () {
                 </div>`;
     member_list.append(html);
   };
-  function appendChatMember(user) {
+  function appendChatMember(user_name, user_id) {
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-                  <input name='group[user_ids][]' type='hidden' value='${user.id}'>
-                  <p class='chat-group-user__name'>${user.name}</p>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                  <p class='chat-group-user__name'>${user_name}</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>`;
     $("#chat-group-users").append(html);
   };
 
-
   $("#user-search-result").on("click", ".chat-group-user", function () {
     $(this).remove();
-    debugger;
+    var user_name = $("a", this).attr("data-user-name");
+    var user_id = $("a", this).attr("data-user-id");
+    appendChatMember(user_name, user_id);
   });
 
-  $("#user-search-field").on("keyup", function () {
+  $(".chat-group-form__input").on("keyup", function () {
     var input = $("#user-search-field").val();
     $.ajax({
       type: 'GET',
@@ -32,7 +33,7 @@ $(function () {
       },
       dataType: 'json'
     })
-      .done(function (users){
+      .done(function (users) {
         member_list.empty();
         if (users.length !== 0) {
           users.forEach(function (user) {
