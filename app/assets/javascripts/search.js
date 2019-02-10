6724fe5757826de454778b37d76e627a1856dded
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-02-10 21:03:34 +0900

    Add processing
    press the add button the element disappears

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -6,9 +6,18 @@ $(function () {
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`;
     member_list.append(html);
-}
+  };
+  function appendChatMember(user) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${user.id}'>
+                  <p class='chat-group-user__name'>${user.name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`;
+    $("#chat-group-users").append(html);
+  };
 
-  $(document).click("user-search-add chat-group-user__btn chat-group-user__btn--add", function () {
+
+  $("#user-search-result").on("click", ".chat-group-user", function () {
     $(this).remove();
     debugger;
   });
