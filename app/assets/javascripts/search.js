84fa6f948db8c23dbdad5d2746127642158ebd2a
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-02-10 22:05:04 +0900

    Add processing
    Refactoring user added functions of group creation function

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -11,15 +11,15 @@ $(function () {
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
                   <input name='group[user_ids][]' type='hidden' value='${user_id}'>
                   <p class='chat-group-user__name'>${user_name}</p>
-                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn' data-user-id = "${user_id}">削除</a>
                 </div>`;
     $("#chat-group-users").append(html);
   };
 
-  $("#user-search-result").on("click", ".chat-group-user", function () {
-    $(this).remove();
-    var user_name = $("a", this).attr("data-user-name");
-    var user_id = $("a", this).attr("data-user-id");
+  $("#user-search-result").on("click", ".chat-group-user__btn--add", function () {
+    var user_name = $(this).data("user-name");
+    var user_id = $(this).data("user-id");
+    $(this).parent().remove()
     appendChatMember(user_name, user_id);
   });
 
@@ -29,6 +29,7 @@ $(function () {
 
   $(".chat-group-form__input").on("keyup", function () {
     var input = $("#user-search-field").val();
+
     $.ajax({
       type: 'GET',
       url: '/users',
