f9537c83f9be84b67c8ab940b9b9bbada3e5dc71
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-02-12 16:43:59 +0900

    Description of javascriptfile
    Write a description for automatic update

--- app/assets/javascripts/interval.js
+++ app/assets/javascripts/interval.js
@@ -1,42 +1,49 @@
 var interval = setInterval(function () {
-  function constructMessageHTML(message) {
+  if (window.location.href.match(/\/groups\/\d+\/messages/)) {
 
-    var image_html = "";
-    if (message.image_present) {
-      image_html = `<img src="${message.image_url}">`;
+    function constructMessageHTML(message) {
+      var image_html = "";
+      if (message.image["url"]) {
+        image_html = `<img src="${message.image.url}">`;
+      }
+      var html = `<div class='chatMain__body--list--message' data-message-id = "${message.id}">
+                    <div class='chatMain__body--list--message--name'>
+                      ${message.user_name}
+                    </div>
+                    <div class='chatMain__body--list--message--time'>
+                      ${message.created_at}
+                    </div>
+                    <div class='chatMain__body--list--message--text'>
+                      <p>${message.content}</p>
+                      ${image_html}
+                    </div>
+                  </div>`;
+      return html;
+    };
+    var message_id = 0;
+    if ($('.chatMain__body--list--message')[0]){
+      message_id = $('.chatMain__body--list--message').last().data('message-id');
     }
-
-    var html = `<div class='chatMain__body--list--message'>
-                  <div class='chatMain__body--list--message--name'>
-                    ${message.user_name}
-                  </div>
-                  <div class='chatMain__body--list--message--time'>
-                    ${message.created_at}
-                  </div>
-                  <div class='chatMain__body--list--message--text'>
-                    <p>${message.content}</p>
-                    ${image_html}
-                  </div>
-                </div>`;
-    return html;
-  };
-
-  $.ajax({
-    url: location.href.json,
-  })
-    .done(function (data) {
-      var html = constructMessageHTML(message);
-      data.messages.forEach(function (message) {
-        if (message.id > id) {
-          $('.chatMain__body--list').append(html)
-        }
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      data: (`id=${message_id}`),
+      dataType: 'json',
+      processData: false
+    })
+      .done(function (data) {
+        $.each(data, function (i, data) {
+          var html = constructMessageHTML(data);
+          $('.chatMain__body--list').append(html);
+        });
+        $('.chatMain__body').animate({
+          scrollTop: $('.chatMain__body')[0].scrollHeight
+        }, 1000, 'swing');
+      })
+      .fail(function (data) {
+        alert('自動更新に失敗しました');
       });
-      $('.chatMain__body').animate({
-        scrollTop: $('.chatMain__body')[0].scrollHeight
-      }, 1000, 'swing');
-  })
-    .fail(function (data) {
-      alert('自動更新に失敗しました');
-   });
-
+  } else {
+    clearInterval(interval);
+  }
 } , 5000);
