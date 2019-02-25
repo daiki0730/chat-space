28cc5fd39763165dfe2570cadd4d7aeaaabc3689
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-02-26 00:29:36 +0900

    test code

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
     respond_to do |format|
       format.html
       format.json {
-          @new_messages = Message.where('id > ? and group_id = ?', params[:id],params[:group_id])
+          @new_messages = @messages.where('id > ? and group_id = ?', params[:id],params[:group_id])
       }
     end
   end
