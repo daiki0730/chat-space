5e6fb8af1667706c60662cb7ab5a8811c4fc8c3a
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-02-25 22:24:45 +0900

    AWS

--- db/schema.rb
+++ db/schema.rb
@@ -35,8 +35,8 @@ ActiveRecord::Schema.define(version: 20190127140707) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["group_id"], name: "index_massages_on_group_id", using: :btree
-    t.index ["user_id"], name: "index_massages_on_user_id", using: :btree
+    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
