7bb8f688deef74bf7bd88691b2ac796f963e83ea
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-02-27 15:55:45 +0900

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
+
 set :application, 'chat-space'
 set :repo_url,  'git@github.com:daiki0730/chat-space.git'
 
