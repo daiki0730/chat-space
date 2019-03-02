b98ce828d97623b4fda44a82893d6621fc6b988b
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-03-02 21:26:17 +0900

    AWS

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,8 @@
 server '54.199.191.186', user: 'ec2-user', roles: %w{app db web}
+
+set :ssh_options, {
+  port: 22,
+  keys: [File.expand_path('~/.ssh/daikey0730.pem')],
+  forward_agent: true,
+  auth_methods: %w(publickey)
+}
