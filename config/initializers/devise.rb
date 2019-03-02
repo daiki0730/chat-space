20a4463b5f8c82048785a5aa48641f9dabc01a41
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-03-02 14:16:21 +0900

    chage code

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,13 +3,18 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
+  # The secret key used by Devise. Devise uses this key to generate
+  # random tokens. Changing this key will render invalid all existing
+  # confirmation, reset password and unlock tokens in the database.
+  config.secret_key = ENV["DEVISE_SECRET_KEY"]
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '8fb040540c056dcd65dce50e1b4660733ad388540dc17122591073448a3f9510974b53f74c09204a696f5adc80147029a808dec8f17b4d53d538fdf125d0fcdb'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
