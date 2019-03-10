7bcbe56e1717efd57360955f06b789ddc3cb03f6
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-03-10 13:30:44 +0900

    delite

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,13 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  IMG_UP_AWS_S3_ACCESS_KEY_ID: ENV["IMG_UP_AWS_S3_ACCESS_KEY_ID"],
-  IMG_UP_AWS_S3_SECRET_ACCESS_KEY: ENV["IMG_UP_AWS_S3_SECRET_ACCESS_KEY"]
-}
-
   config.fog_directory  = 'raccoon0730'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/raccoon0730'
 end
